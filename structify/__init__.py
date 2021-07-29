3a0bbb0575957ad548fe53ca053fbe1cfb24512c
Atul Bhouraskar <atul@yescourse.com>
2021-07-29 15:29:25 +1000

    Update to python3.8, simplify metaclass

--- structify/__init__.py
+++ structify/__init__.py
@@ -1,6 +1,7 @@
-from __future__ import absolute_import, unicode_literals
+from __future__ import annotations
 
 import struct
+from typing import Dict, IO, List, Optional, Type, Union
 
 INT8_T = 'b'
 UINT8_T = 'B'
@@ -20,215 +21,182 @@ BYTE_ORDER_NETWORK = '!'
 
 DEFAULT_BYTE_ORDER = BYTE_ORDER_LITTLE_ENDIAN
 
-ERROR_MSG = 'StructFields can only be used within Struct objects'
+FieldValueType = Union[int, str]
 
 
 class StructException(Exception):
     pass
 
 
-class StructField(object):
+class StructField:
     # used to track ordering of fields within a struct
-    count_ = 0  # type: int
+    count_: int = 0
 
-    def __init__(self, byte_size, default=None, strlen=None):
-        if byte_size == CHAR and not strlen:
+    def __init__(self, field_type, default: Optional[FieldValueType] = None,
+                 str_len: Optional[int] = None):
+        if field_type == CHAR and not str_len:
             raise StructException('CHAR fields are fixed width and require '
                                   'strlen to be specified')
-        self.byte_size = byte_size  # type: str
-        self.default = default  # type: any
-        self.strlen = strlen  # type: int
-        self.count = StructField.count_  # type: int
+        self.field_type: str = field_type
+        self.default: Optional[FieldValueType] = default
+        self.str_len: int = str_len
+        # record count of this instance
+        self.count: int = StructField.count_
         StructField.count_ += 1
+        self.index: int = 0  # set by MetaStruct for each instance
         # stores the name of the attribute that this field is connected to
-        # will be set by MetaStruct during Struct creation
-        self.name = None  # type: str
+        # will be set by __set_name__ during class creation
+        self.name = ''  # type: str
 
     # descriptor interface
-    def __get__(self, instance, owner):
-        if not self.name:
-            raise RuntimeError(ERROR_MSG)
-        return getattr(instance.data_, self.name, self.default)
+    def __get__(self, instance: Struct, owner):
+        if not instance:
+            return self
+        return instance.get_field_value(self.name)
 
-    def __set__(self, instance, value):
-        if not self.name:
-            raise RuntimeError(ERROR_MSG)
-        setattr(instance.data_, self.name, value)
+    def __set__(self, instance: Struct, value):
+        instance.set_field_value(self.name, value)
 
-
-class StructBase(object):
-    pass
+    def __set_name__(self, owner, name: str):
+        self.name = name
 
 
-def recurse_struct_fields(cls, fields):
-    for name, field in cls.__dict__.items():
-        if isinstance(field, StructField):
-            field.name = name
-            fields.append(field)
-    for base in cls.__bases__:
-        if issubclass(base, StructBase):
-            recurse_struct_fields(base, fields)
+def int8_t(default: int = None):
+    return StructField(INT8_T, default=default)
+
+
+def uint8_t(default: int = None):
+    return StructField(UINT8_T, default=default)
+
+
+def int16_t(default: int = None):
+    return StructField(INT16_T, default=default)
+
+
+def uint16_t(default: int = None):
+    return StructField(UINT16_T, default=default)
+
+
+def int32_t(default: int = None):
+    return StructField(INT32_T, default=default)
+
+
+def uint32_t(default: int = None):
+    return StructField(UINT32_T, default=default)
+
+
+def int64_t(default: int = None):
+    return StructField(INT64_T, default=default)
+
+
+def uint64_t(default: int = None):
+    return StructField(UINT64_T, default=default)
+
+
+def char(strlen: int, default: str = None):
+    return StructField(CHAR, default=default, str_len=strlen)
 
 
 class MetaStruct(type):
-    def __init__(cls, name, bases, dct):
-        fields = []  # type: List[StructField]
-        recurse_struct_fields(cls, fields)
+    def __new__(mcs, name, bases, attrs):
+        new_class: Type[Struct] = super().__new__(mcs, name, bases, attrs)
 
-        if fields:
-            fields = sorted(fields, key=lambda f: f.count)
+        # Walk through the MRO to find all StructField instances
+        struct_fields = {}
+        for base in reversed(new_class.__mro__):
+            for attr, value in base.__dict__.items():
+                if isinstance(value, StructField):
+                    struct_fields[attr] = value
 
-            if 'byte_order' in dct:
-                byte_order = cls.byte_order
-            else:
-                byte_order = DEFAULT_BYTE_ORDER
+        new_class._DEFAULT_VALUES = []
+        new_class._NAME_INDEX_MAP = {}
 
-            # calculate the format
-            fmt = ''
-            for field in fields:  # type: StructField
-                byte_size = field.byte_size
-                if byte_size == CHAR:
-                    fmt += str(field.strlen)
-                fmt += byte_size
+        # sort the fields in declaration order
+        field_list = sorted(struct_fields.values(), key=lambda f: f.count)
+        # calculate the format and field map
+        fmt = ''
+        field: StructField
+        for index, field in enumerate(field_list):
+            field_type = field.field_type
+            if field_type == CHAR:
+                fmt += str(field.str_len)
+            fmt += field_type
+            field.index = index
+            new_class._NAME_INDEX_MAP[field.name] = index
+            new_class._DEFAULT_VALUES.append(field.default)
 
-            base_fmt = fmt
-            fmt = byte_order + fmt
+        new_class._BASE_STRUCT_FORMAT = fmt
+        new_class._STRUCT_FORMAT = getattr(
+            new_class, 'BYTE_ORDER', DEFAULT_BYTE_ORDER
+        ) + fmt
+        new_class._PACKER = struct.Struct(new_class._STRUCT_FORMAT)
 
-            class StructData(object):
-                __slots__ = [field.name for field in fields]
-                packer = struct.Struct(fmt)
-
-                def pack(self):
-                    values = [getattr(self, f.name) for f in fields]
-                    return self.packer.pack(*values)
-
-                def pack_endian(self, byte_order_):
-                    values = [getattr(self, f.name) for f in fields]
-                    return struct.pack(byte_order_ + base_fmt, *values)
-
-                def unpack_values_(self, values):
-                    index = 0
-                    for f in fields:
-                        setattr(self, f.name, values[index])
-                        index += 1
-
-                def unpack(self, string):
-                    values = self.packer.unpack(string)
-                    self.unpack_values_(values)
-
-                def unpack_endian(self, string, byte_order_):
-                    values = struct.unpack(byte_order_ + base_fmt, string)
-                    self.unpack_values_(values)
-
-            # This will be the __init__ function for the class we are creating
-            def init(obj, **kwargs):
-                # ensure that the struct data object is created
-                obj.data_ = StructData()
-
-                # init values from kwargs if supplied
-                for f in fields:
-                    fname = f.name
-                    if fname in kwargs:
-                        setattr(obj, fname, kwargs.pop(fname))
-                if kwargs:
-                    raise StructException(
-                        'Unexpected kwargs passed to {}: {}'.format(
-                            name, kwargs
-                        )
-                    )
-
-            # override the __init__ for the Struct subclass
-            cls.__init__ = init
-
-        super(MetaStruct, cls).__init__(name, bases, dct)
+        return new_class
 
 
-class Struct(StructBase):
-    __metaclass__ = MetaStruct
+class Struct(metaclass=MetaStruct):
+    # Child classes can override this to set preferred byte order
+    BYTE_ORDER = DEFAULT_BYTE_ORDER
+
+    # Private class vars, will be overridden by metaclass
+    _BASE_STRUCT_FORMAT = ''
+    _STRUCT_FORMAT = ''
+    _NAME_INDEX_MAP: Dict[str, int] = {}
+    _DEFAULT_VALUES: List = []
+    _PACKER: struct.Struct = struct.Struct(_STRUCT_FORMAT)
 
     def __init__(self, **kwargs):
-        pass
+        # initialise values list to defaults
+        self.values: list = self._DEFAULT_VALUES.copy()
 
-    def pack(self):
-        return self.data_.pack()
+        if kwargs:
+            # initialise any values passed in via kwargs
+            for name, index in self._NAME_INDEX_MAP.items():
+                if name in kwargs:
+                    self.values[index] = kwargs.pop(name)
 
-    def pack_endian(self, byte_order):
-        return self.data_.pack_endian(byte_order)
+            if kwargs:
+                raise StructException(f'Unexpected kwargs {kwargs}')
 
-    def unpack(self, string):
-        self.data_.unpack(string)
+    def get_field_value(self, name: str):
+        return self.values[self._NAME_INDEX_MAP[name]]
 
-    def unpack_endian(self, string, byte_order):
-        self.data_.unpack_endian(string, byte_order)
+    def set_field_value(self, name: str, value):
+        self.values[self._NAME_INDEX_MAP[name]] = value
+
+    def pack(self) -> bytes:
+        return self._PACKER.pack(*self.values)
+
+    def pack_endian(self, byte_order: str) -> bytes:
+        return struct.pack(byte_order + self._BASE_STRUCT_FORMAT, *self.values)
+
+    def unpack(self, bytes_: bytes):
+        self.values = self._PACKER.unpack(bytes_)
+
+    def unpack_endian(self, bytes_: bytes, byte_order: str):
+        self.values = struct.unpack(byte_order + self._BASE_STRUCT_FORMAT,
+                                    bytes_)
 
     def __len__(self):
-        return self.data_.packer.size
+        return self._PACKER.size
 
     def sizeof(self):
         return self.__len__()
 
 
-class FileHelper(object):
+class FileHelper:
     def __init__(self, fp):
         if 'b' not in fp.mode:
             raise StructException('File must be opened in binary mode')
-        self.fp = fp  # type: file
+        self.fp: IO = fp
 
     def read_into(self, struct_obj):
         size = struct_obj.sizeof()
-        string = self.fp.read(size)
-        struct_obj.unpack(string)
+        bytes_: bytes = self.fp.read(size)
+        if len(bytes_) < size:
+            return False
+        struct_obj.unpack(bytes_)
+        return True
 
     def write(self, struct_obj):
         return self.fp.write(struct_obj.pack())
-
-
-def int8_t(default=None):
-    return StructField(INT8_T, default=default)
-
-
-def uint8_t(default=None):
-    return StructField(UINT8_T, default=default)
-
-
-def int16_t(default=None):
-    return StructField(INT16_T, default=default)
-
-
-def uint16_t(default=None):
-    return StructField(UINT16_T, default=default)
-
-
-def int32_t(default=None):
-    return StructField(INT32_T, default=default)
-
-
-def uint32_t(default=None):
-    return StructField(UINT32_T, default=default)
-
-
-def int64_t(default=None):
-    return StructField(INT64_T, default=default)
-
-
-def uint64_t(default=None):
-    return StructField(UINT64_T, default=default)
-
-
-def char(strlen, default=None):
-    return StructField(CHAR, default=default, strlen=strlen)
-
-
-class TestStruct(Struct):
-    a = uint8_t()
-    b = int64_t(34)
-
-
-class SubTestStruct(TestStruct):
-    c = int8_t()
-
-
-class AnotherStruct(Struct):
-    byte_order = '>'
-    x = int8_t()
-    y = uint64_t(8989)
